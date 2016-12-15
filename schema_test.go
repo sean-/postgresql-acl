9351d61f1b27e2638bc4f15b5ca8ece0116d09a7
Sean Chittenden <sean@chittenden.org>
2016-12-15 07:49:37 -0800

    Begin parsing out PostgreSQL's schema ACLs.

---
+++ schema_test.go
@@ -0,0 +1,122 @@
+package pgacl_test
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/sean-/pgacl"
+)
+
+func TestSchemaString(t *testing.T) {
+	tests := []struct {
+		name string
+		in   string
+		out  string
+		want pgacl.Schema
+		fail bool
+	}{
+		{
+			name: "default",
+			in:   "foo=",
+			out:  "foo=",
+			want: pgacl.Schema{Role: "foo"},
+		},
+		{
+			name: "all without grant",
+			in:   "foo=UC",
+			out:  "foo=UC",
+			want: pgacl.Schema{
+				Role:   "foo",
+				Create: true,
+				Usage:  true,
+			},
+		},
+		{
+			name: "all with grant",
+			in:   "foo=U*C*",
+			out:  "foo=U*C*",
+			want: pgacl.Schema{
+				Role:        "foo",
+				Create:      true,
+				CreateGrant: true,
+				Usage:       true,
+				UsageGrant:  true,
+			},
+		},
+		{
+			name: "all mixed grant1",
+			in:   "foo=U*C",
+			out:  "foo=U*C",
+			want: pgacl.Schema{
+				Role:       "foo",
+				Create:     true,
+				Usage:      true,
+				UsageGrant: true,
+			},
+		},
+		{
+			name: "all mixed grant2",
+			in:   "foo=UC*",
+			out:  "foo=UC*",
+			want: pgacl.Schema{
+				Role:        "foo",
+				Create:      true,
+				CreateGrant: true,
+				Usage:       true,
+			},
+		},
+		{
+			name: "public all",
+			in:   "=U*C*",
+			out:  "=U*C*",
+			want: pgacl.Schema{
+				Role:        "",
+				Create:      true,
+				CreateGrant: true,
+				Usage:       true,
+				UsageGrant:  true,
+			},
+		},
+		{
+			name: "invalid input1",
+			in:   "bar*",
+			want: pgacl.Schema{},
+			fail: true,
+		},
+		{
+			name: "invalid input2",
+			in:   "%",
+			want: pgacl.Schema{},
+			fail: true,
+		},
+	}
+
+	for i, test := range tests {
+		if test.name == "" {
+			t.Fatalf("test %d needs a name", i)
+		}
+
+		t.Run(test.name, func(t *testing.T) {
+			got, err := pgacl.NewSchema(test.in)
+			if err != nil && !test.fail {
+				t.Fatalf("unable to parse schema ACL %+q: %v", test.in, err)
+			}
+
+			if err == nil && test.fail {
+				t.Errorf("expected failure")
+			}
+
+			if test.fail && err != nil {
+				return
+			}
+
+			if out := test.want.String(); out != test.out {
+				t.Errorf("want %+q got %+q", test.out, out)
+			}
+
+			if !reflect.DeepEqual(test.want, got) {
+				t.Errorf("bad: expected %v to equal %v", test.want, got)
+			}
+		})
+	}
+}
